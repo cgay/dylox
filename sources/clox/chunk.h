237784ad9c71cbadb10a6aa419327a2f16979254
Carl Gay <carlgay@gmail.com>
2024-06-28 13:26:54 -0400

    Basic VM - through section 15.1

--- sources/clox/chunk.h
+++ sources/clox/chunk.h
@@ -4,19 +4,24 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+  OP_CONSTANT,
   OP_RETURN,
 } OpCode;
 
 typedef struct {
-  int count;
-  int capacity;
-  uint8_t* code;
+  int count;              /* num bytes in chunk */
+  int capacity;           /* allocated bytes in `code` */
+  uint8_t* code;          /* the byte code */
+  int* lines;             /* line numbers, 1-to-1 correspondence with `code` */
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 void freeChunk(Chunk* chunk);
 
 #endif
--- sources/clox/main.c
+++ sources/clox/main.c
@@ -1,12 +1,21 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
-  writeChunk(&chunk, OP_RETURN);
+
+  int index = addConstant(&chunk, 1.2); /* for now values are only doubles */
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, index, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
